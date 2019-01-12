39190f5e8dab338a86be36f7f6911059cd512a63
Frederik Möllers <frederik@die-sinlosen.de>
2019-01-12 15:23:08 +0100

    Begin evaluation component

---
+++ frontend/src/app/evaluation/views/evaluation-view/evaluation-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pf-evaluation-view',
+  templateUrl: './evaluation-view.component.html',
+  styleUrls: ['./evaluation-view.component.css'],
+})
+export class EvaluationViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
