8209f355d07d48dd67673bbfbba0d3e846f82090
Alexander Keller <alexander.keller@centigrade.de>
2019-01-12 15:22:01 +0100

    added input and outputs for MultipleChoiceComponent

--- frontend/src/app/quiz/components/multiple-choice/multiple-choice.component.ts
+++ frontend/src/app/quiz/components/multiple-choice/multiple-choice.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Answer } from '../../types/answer';
+import { Question } from '../../types/question';
 
 @Component({
   selector: 'pf-multiple-choice',
   templateUrl: './multiple-choice.component.html',
   styleUrls: ['./multiple-choice.component.css'],
 })
-export class MultipleChoiceComponent implements OnInit {
-  constructor() {}
+export class MultipleChoiceComponent {
+  /**
+   * Question will be displayed as question itself and the including answer possibilities.
+   */
+  @Input() question: Question;
 
-  ngOnInit() {}
+  /**
+   * Occured if the answered the question. The score of the selected answer will be emitted.
+   */
+  @Output() answer = new EventEmitter<number>();
+
+  // properties
+  public selectedAnswer: Answer;
+
+  //methods
+  public confirm() {
+    this.answer.emit(this.selectedAnswer.score);
+  }
 }
