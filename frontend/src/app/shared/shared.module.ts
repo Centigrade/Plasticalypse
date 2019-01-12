7d342c3ebe06cce5c5168f8c848859218f75d1b8
David Würfel <david.wuerfel@centigrade.de>
2019-01-12 20:31:32 +0100

    reuse submit button in family quiz

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SubmitButtonComponent } from './components/submit-button/submit-button.component';
+import { IconPipe } from './pipes/icon.pipe';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  declarations: [SubmitButtonComponent, IconPipe],
+  exports: [SubmitButtonComponent, IconPipe],
 })
-export class SharedModule { }
+export class SharedModule {}
