984d954eada9974616b0f49b9b357a7f2389c98f
Alexander Keller <alexander.keller@centigrade.de>
2019-01-13 12:12:47 +0100

    added new url and port in service

--- frontend/src/app/core/services/profile.service.ts
+++ frontend/src/app/core/services/profile.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserLevel } from './../../shared/types/user-level';
 
-const BASE_API_URL = 'http://10.8.29.15';
+const BASE_API_URL = 'http://10.8.29.15:8000';
 
 @Injectable()
 export class ProfileService {
@@ -23,6 +23,6 @@ export class ProfileService {
     const url = `${BASE_API_URL}/user/update`;
 
     // send the data to the given API_URL
-    this.http.post(url, body);
+    this.http.post(url, body).subscribe();
   }
 }
