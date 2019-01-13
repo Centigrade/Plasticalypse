f29a27e974ef5c0b5ac0ac890ca9143ab6bc4933
Kai-Dominik Kuhn <Dominik.Kuhn@Centigrade.de>
2019-01-13 16:49:06 +0100

    fixes

--- frontend/src/app/groceries/view/grocery-view/grocery-view.component.ts
+++ frontend/src/app/groceries/view/grocery-view/grocery-view.component.ts
@@ -10,11 +10,7 @@ import { GroceryHistory } from '../../types/grocery-history';
   styleUrls: ['./grocery-view.component.scss'],
 })
 export class GroceryViewComponent {
-<<<<<<< HEAD
   constructor(private router: Router, private groceryService: GroceryService) {
-=======
-  constructor(private groceryService: GroceryService, private router: Router) {
->>>>>>> 69c5d2cbf79a68b4597bcc237fe30c6d7c621e90
     this.groceryService.getGroceryOptions().subscribe((groceries: Grocery[]) => {
       groceries.map(grocery => (grocery.counter = 0));
 
