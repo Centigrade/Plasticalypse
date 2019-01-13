5c1f27e2563b5df459543b73d4013ba762783e0b
David Würfel <david.wuerfel@centigrade.de>
2019-01-13 17:21:49 +0100

    fix ts lint error

--- frontend/src/app/profile/components/expander/expander.component.ts
+++ frontend/src/app/profile/components/expander/expander.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pf-expander',
   templateUrl: './expander.component.html',
   styleUrls: ['./expander.component.scss'],
 })
-export class ExpanderComponent implements OnInit {
+export class ExpanderComponent {
   @Input() title: string;
 
   public isOpen = false;
