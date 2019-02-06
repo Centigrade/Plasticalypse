8db6a2238c384b50af9f7329ab530fb5f7e93350
Kai-Dominik Kuhn <Dominik.Kuhn@Centigrade.de>
2019-02-06 14:16:51 +0100

    feat: implement login via login-dialog

--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
+import { LoginDialogComponent } from './views/login-dialog/login-dialog.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, LoginDialogComponent],
   imports: [CommonModule, LoginRoutingModule, SharedModule],
 })
 export class LoginModule {}
