ffb40067aa31a2ef22fbf020d2f77c0af5ee173c
David Würfel <david.wuerfel@centigrade.de>
2020-03-24 09:53:10 +0100

    chore: remove storybook addons and adjust src path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,3 @@
 module.exports = {
-  stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+  stories: ['../frontend/src/**/*.stories.ts'],
 };
